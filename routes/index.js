cf1d49d796af74b09720be82ea8fe052931a675b
Mateusz <mateuszzygadlo00@gmail.com>
2021-11-05 23:33:22 +0100

    Adding startPage styles

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => res.render('index'));
 
-router.get('/sign-in', (req, res) => res.render('sign-in_form'));
+router.get('/log-in', (req, res) => res.render('log-in_form'));
 router.get('/sign-up', (req, res) => res.render('sign-up_form'));
 
 module.exports = router;
